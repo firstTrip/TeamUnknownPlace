e5c3a1a875dbb3cd5532a171e88df095b91961a3
Kim JinSoo <zxc29321@gmail.com>
2020-12-13 00:32:33 +0900

    WalkSound 추가
     정지상태가 아닐때만 발동하게 해주세요.

--- Assets/_Main/_HWJ/Scripts/Player.cs
+++ Assets/_Main/_HWJ/Scripts/Player.cs
@@ -17,6 +17,15 @@ public class Player : MonoBehaviour
     [SerializeField]private float JumpForce;
     [SerializeField] private float moveForce;
     [SerializeField] private float moveSpeed;
+
+    #region WalkSound
+
+    [Header("발소리 쿨다운")]public float WalkSoundCoolDown = 0.2f;
+    private float WalkSoundCoolDownNow = 0f;
+
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +35,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        WalkSoundCoolDownCheck();
+
         InputManager();
         Move();
 
@@ -72,7 +83,32 @@ public class Player : MonoBehaviour
             else if (rb.velocity.x < moveForce * (-1))
                 rb.velocity = new Vector2(moveForce * (-1), rb.velocity.y);
         }
-       
+
+        // 정지상태가 아닐때만 발동하게 해주세요.
+        WalkSound();
+
+    }
+
+    private void WalkSoundCoolDownCheck()
+    {
+        if (WalkSoundCoolDownNow > 0)
+        {
+            WalkSoundCoolDownNow -= Time.deltaTime;
+        }
+    }
+
+    private void WalkSound()
+    {
+        #region WalkSound
+        if (WalkSoundCoolDownNow <= 0)
+        {
+            AudioManager.Instance.PlayOneShot("Footstep");
+            EffectManager.Instance.SetPool("SoundWave", transform.position, new Vector3(0.5f, 0.5f, 1f));
+            LightManager.Instance.LightCheckSound(transform.position, 2);
+
+            WalkSoundCoolDownNow = WalkSoundCoolDown;
+        }
+        #endregion
     }
 
     private void Jump()
