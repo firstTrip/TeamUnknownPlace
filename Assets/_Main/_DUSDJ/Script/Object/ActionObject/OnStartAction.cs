b0535d82eb1f9b94d5907198660f060df9d66188
Kim JinSoo <zxc29321@gmail.com>
2020-12-19 05:18:17 +0900

    중간저장

---
+++ Assets/_Main/_DUSDJ/Script/Object/ActionObject/OnStartAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class OnStartAction : MonoBehaviour
+{
+    public string Message;
+    public GameObject Target;
+    
+    private void Start()
+    {
+        Target.SendMessage(Message);
+    }
+
+
+}
